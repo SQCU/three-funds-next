84aca37d3cf47f0ae17b3fd9373338783e02141b
SQCU <70545818+SQCU@users.noreply.github.com>
2021-04-20 15:47:06 -0700

    remove deprecated methods

--- threejs/three-funds.js
+++ threejs/three-funds.js
@@ -19,51 +19,6 @@ function main()	{
 	const scene = new THREE.Scene();
 	scene.background = new THREE.Color(0xAAAABB);
 	
-	//cube block
-	const boxW = 1;
-	const boxH = 1;
-	const boxD = 1;
-	const geom = new THREE.BoxGeometry(boxW, boxH, boxD);
-
-	
-	function cuber(geometry, color, x){
-		const material = new THREE.MeshPhongMaterial({color});
-		const cube = new THREE.Mesh(geometry, material);
-		scene.add(cube);
-		
-		cube.position.x = x;
-		return cube;//here's your cube good sir
-	}
-	
-	const cubes =[
-	cuber(geom, 0xC7E8C1, -2),
-	cuber(geom, 0xE8C7C1, 0),
-	cuber(geom, 0xC1C7E8, 2)
-	];
-	
-	//let's add some circle sections!
-	const cradius = .5;
-	const csegments = 9;
-	const cthetastart = Math.PI*4/9;
-	const cthetalength = Math.PI*2/9;
-	const cgeometry = new THREE.CircleGeometry(
-    cradius, csegments, cthetastart, cthetalength);
-	
-	function circler(geometry, color, x, y){	//deprecated by mattescrambler addsolidgomm addobject
-		const material = new THREE.MeshPhongMaterial({side: THREE.DoubleSide, color});	//oops we need side:DoubleSide lol
-		const circ = new THREE.Mesh(geometry, material);
-		scene.add(circ);
-		
-		circ.position.x = x;
-		circ.position.y = y;
-		return circ;//here's your ARC good sir
-	}
-	const circs = [
-	circler (cgeometry, 0xE8E8C1, -2, 1.2),
-	circler (cgeometry, 0xC1E8E8, 0, 1.2),
-	circler (cgeometry, 0xE8C1E8, 2, 1.2),
-	];
-	
 	const objectsman = [];	//this is where all of our geometry from addsolidgom gets dumped
 	const dispersion = 15;
 	
@@ -147,16 +102,6 @@ function render(time) {
 	camera.aspect = canvas.clientWidth / canvas.clientHeight;
 	camera.updateProjectionMatrix();
 	}
-	
-	//deprecated, please remove
-	cubes.forEach((cube) => {
-	cube.rotation.x = time;
-	cube.rotation.y = time;
-	});
-	//deprecated, please remove
-	circs.forEach((circ) => {
-	circ.rotation.y = time*7/4;
-	});
 	
 	objectsman.forEach((obj) => {
 		obj.rotation.x = time;
